f93cf48e01e5e88bd4c33ba0c8c77916832c3f6a
stein01zx-afk <stein01zx@gmail.com>
2026-07-28 04:01:14 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express=require('express');
+const http=require('http');
+const {Server}=require('socket.io');
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server);
+app.use(express.static('public'));
+io.on('connection',s=>{
+  s.emit('msg',{platform:'SYSTEM',user:'Servidor',text:'Conectado'});
+  s.on('fake',m=>io.emit('msg',m));
+});
+server.listen(3000,()=>console.log('http://localhost:3000'));
