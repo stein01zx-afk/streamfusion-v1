3d230bab4626b4b8606717f965017e10ed501252
stein01zx-afk <stein01zx@gmail.com>
2026-07-28 15:54:23 -0500

    Update tiktok.js

--- services/tiktok.js
+++ services/tiktok.js
@@ -1,74 +1,24 @@
-const { WebcastPushConnection } = require("tiktok-live-connector");
+import {
+    TikTokLiveConnection,
+    WebcastEvent,
+    ControlEvent
+} from "tiktok-live-connector";
 
 let connection = null;
 
-async function connect(username, io) {
+export async function connect(username, io) {
 
     if (connection) {
         try {
-            connection.disconnect();
+            await connection.disconnect();
         } catch {}
     }
 
-    connection = new WebcastPushConnection(username);
-
-    connection.on("chat", data => {
-
-        io.emit("chat", {
-            platform: "tiktok",
-            type: "chat",
-            user: data.nickname,
-            uniqueId: data.uniqueId,
-            message: data.comment
-        });
-
+    connection = new TikTokLiveConnection(username, {
+        signApiKey: process.env.EULER_API_KEY
     });
 
-    connection.on("gift", data => {
-
-        io.emit("chat", {
-            platform: "tiktok",
-            type: "gift",
-            user: data.nickname,
-            uniqueId: data.uniqueId,
-            gift: data.giftName,
-            amount: data.repeatCount
-        });
-
-    });
-
-    connection.on("like", data => {
-
-        io.emit("chat", {
-            platform: "tiktok",
-            type: "like",
-            user: data.nickname,
-            likes: data.likeCount
-        });
-
-    });
-
-    connection.on("follow", data => {
-
-        io.emit("chat", {
-            platform: "tiktok",
-            type: "follow",
-            user: data.nickname
-        });
-
-    });
-
-    connection.on("share", data => {
-
-        io.emit("chat", {
-            platform: "tiktok",
-            type: "share",
-            user: data.nickname
-        });
-
-    });
-
-    connection.on("connected", () => {
+    connection.on(ControlEvent.CONNECTED, () => {
 
         io.emit("system", {
             message: "TikTok conectado."
@@ -76,7 +26,7 @@ async function connect(username, io) {
 
     });
 
-    connection.on("disconnected", () => {
+    connection.on(ControlEvent.DISCONNECTED, () => {
 
         io.emit("system", {
             message: "TikTok desconectado."
@@ -84,10 +34,66 @@ async function connect(username, io) {
 
     });
 
-    connection.on("error", error => {
+    connection.on(ControlEvent.ERROR, ({ info, exception }) => {
 
         io.emit("system", {
-            message: error.toString()
+            message: exception?.message || info || "Error de TikTok"
+        });
+
+    });
+
+    connection.on(WebcastEvent.CHAT, (data) => {
+
+        io.emit("chat", {
+            platform: "tiktok",
+            type: "chat",
+            user: data.user?.nickname,
+            uniqueId: data.user?.uniqueId,
+            message: data.comment
+        });
+
+    });
+
+    connection.on(WebcastEvent.GIFT, (data) => {
+
+        io.emit("chat", {
+            platform: "tiktok",
+            type: "gift",
+            user: data.user?.nickname,
+            uniqueId: data.user?.uniqueId,
+            gift: data.giftDetails?.giftName ?? data.giftId,
+            amount: data.repeatCount
+        });
+
+    });
+
+    connection.on(WebcastEvent.LIKE, (data) => {
+
+        io.emit("chat", {
+            platform: "tiktok",
+            type: "like",
+            user: data.user?.nickname,
+            likes: data.likeCount
+        });
+
+    });
+
+    connection.on(WebcastEvent.FOLLOW, (data) => {
+
+        io.emit("chat", {
+            platform: "tiktok",
+            type: "follow",
+            user: data.user?.nickname
+        });
+
+    });
+
+    connection.on(WebcastEvent.SHARE, (data) => {
+
+        io.emit("chat", {
+            platform: "tiktok",
+            type: "share",
+            user: data.user?.nickname
         });
 
     });
@@ -96,13 +102,17 @@ async function connect(username, io) {
 
 }
 
-function disconnect() {
+export async function disconnect() {
 
-    if (connection) {
+    if (!connection) return;
 
-        try {
-            connection.disconnect();
-        } catch {}
+    try {
+        await connection.disconnect();
+    } catch {}
+
+    connection = null;
+
+}        } catch {}
 
         connection = null;
 
