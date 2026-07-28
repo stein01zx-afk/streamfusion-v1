b32763dfafd78aae6becfb2860b0e381e90a7375
stein01zx-afk <stein01zx@gmail.com>
2026-07-28 04:22:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {Server}=require('socket.io');
 const app=express();
 const server=http.createServer(app);
 const io=new Server(server);
-app.use(express.static('public'));
+app.use(express.static('Public'));
 io.on('connection',s=>{
   s.emit('msg',{platform:'SYSTEM',user:'Servidor',text:'Conectado'});
   s.on('fake',m=>io.emit('msg',m));
